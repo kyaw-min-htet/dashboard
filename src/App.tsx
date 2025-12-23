0a01e606e29ed541ad4a3b6b222185b0f7f3229c
Kyaw Min Htet <maungthant@Kyaws-Air.lan>
2025-12-23 10:28:51 +0700

    remove unnecessary file -> done

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import DashbordLayout from "./layouts/DashbordLayout";
 import { Routes, Route } from "react-router-dom";
 import Contacts from "./pages/Contacts";
-import Dashboard from "./pages/Dashboard";
 import Organizations from "./pages/Organizations";
 import Report from "./pages/Reports";
 import CreateOrganization from "./pages/CreateOrganization";
@@ -28,7 +27,6 @@ const App = () => {
             <DashbordLayout />
           </ProtectedRoute>
         }>
-          <Route index element={<Dashboard />} />
           <Route path="contacts" element={<Contacts />} />
           <Route path="create-contact" element={<CreateContacts />} />
           <Route path="organizations" element={<Organizations />} />
